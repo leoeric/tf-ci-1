00781a9207aab3793dbf18345c16190ba15ab6ef
leoeric <leoeric13@gmail.com>
2025-07-10 20:37:59 +0800

    backend2 comment

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = "eric-ce10-tfstate"  #Change this
+    key    = "eric-ce10-tfstate" #Change this
     region = "ap-southeast-1"
   }
 }
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix = "eric"# Set your bucket name here
+  bucket_prefix = "eric" # Set your bucket name here
 }
