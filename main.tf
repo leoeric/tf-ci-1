3c6c4b14e2ae1ea2f80c82e1d14591de622c46f6
leoeric <leoeric13@gmail.com>
2025-07-10 20:18:08 +0800

    add new s3 bucket

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce10-tfstate"
+    key    = "eric-ce10-tfstate"  #Change this
+    region = "ap-southeast-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket_prefix = "eric"# Set your bucket name here
+}
